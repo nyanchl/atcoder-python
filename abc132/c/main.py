70f8c9861963f7cc6a69000627e2f97ced2507f9
Nyanchl <rotdicm@gmail.com>
2024-08-22 08:29:25 +0900

    abc132 c

--- abc132/c/main.py
+++ abc132/c/main.py
@@ -1,2 +1,10 @@
-import math
-from collections import defaultdict
+N = int(input())
+d = list(map(int, input().split()))
+d.sort()
+count = 0
+half_line = d[N//2]
+if len(d[:d.index(half_line)]) == len(d[d.index(half_line):]):
+    count += 1
+    if d[d.index(half_line)] - d[d.index(half_line)-1] != 0 or 1:
+        count += d[d.index(half_line)] - d[d.index(half_line)-1] -1
+print(count)
